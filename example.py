097817d91f98bc8000d4667484ca757bf8bec346
demdecuong <nguyenphucminh2804@gmail.com>
2021-06-07 15:42:40 +0700

    update readme

--- example.py
+++ example.py
@@ -1,5 +1,10 @@
+import time
 from src.extractor import Extractor
 extractor = Extractor()
 
+start = time.time()
 text2 = "tôi tên là pham hai nam"
 print(extractor.extract_person_name(text2,'pattern'))
+print(time.time() - start) 
+# pattern : 0.00014662742614746094
+# dict + pattern : 0.0001785755157470703
