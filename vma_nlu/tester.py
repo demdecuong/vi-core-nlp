22d5d328aa7ae51332bb47b20110d95e1dc16778
Trần Hoàng Vũ (VNCDLL-CTÐTKSAI) <vuth5@vingroup.net>
2021-06-15 15:51:17 +0700

    update tester

--- vma_nlu/tester.py
+++ vma_nlu/tester.py
@@ -1,13 +1,17 @@
 import datetime
+
+from nltk import text
 from vma_nlu.ner.extractor import Extractor
 from vma_nlu.testcase.test_cases import get_person_name_tc, get_time_tc
 from termcolor import colored
+from vma_nlu.testcase.test_case_date import get_date_test_case
 
 class Tester:
     def __init__(self):
         self.extractor = Extractor(load_dict=False)    
         self.per_tc, self.per_expected_result = get_person_name_tc()
         self.time_tc, self.time_expected_result, self.time_spec_tc, self.time_spec_expected_result = get_time_tc()
+        self.date_tc, self.date_expected_result = get_date_test_case()
     def test_person_name(self):
         tc_num = 1
         total_acc = 0 
@@ -58,7 +62,33 @@ class Tester:
         self.write_result('Total',total_acc,total_len)
 
     def test_date(self):
-        pass
+        correct = 0
+        len_total_test = 0
+        tc_num = 1
+        for tc, ex_result in zip(self.date_tc, self.date_expected_result):
+            len_total_test += len(tc)
+            correct_tc = 0
+            for test, target in zip(tc, ex_result):
+                # print(test)
+                pred = self.extractor.extract_date(test)
+                # print(pred)
+                entitites  = pred['entities']
+                result = []
+                for i in entitites:
+                    value = i['value']
+                    result.extend(value)
+
+                if result == target:
+                    correct += 1
+                    correct_tc += 1
+            len_tc = len(tc)
+            self.write_result(tc=tc_num, correct=correct_tc, total=len_tc)
+            tc_num += 1
+
+        acc = correct/len_total_test
+        return acc
+            
+
 
     def test_intent(self):
         pass
