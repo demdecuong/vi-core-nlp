85ec123ff80be06813a9f0b7cc99700ee4007d50
Pham Nam <namph@ftech.ai>
2021-06-18 16:14:01 +0700

    Bumped version number to 1.0.8

--- example.py
+++ example.py
@@ -63,5 +63,5 @@ extractor = Extractor(load_dict=False) # True: Load dictionary path
 # text = '3 giờ 20 phút kế tiếp'
 # print(extractor.extract_ner(text,'inform'))
 
-text = "31"
+text = "31/06/1995"
 print(extractor.extract_date(text))
