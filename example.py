c6c0dc73bbe000f16678883bdb11bc889a45618e
Pham Nam <namph@ftech.ai>
2021-07-07 16:33:27 +0700

    update requirements

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 from vma_nlu.ner.extractor import Extractor
 from vma_nlu.tester import Tester
+from vma_nlu.ner.pername_deeplearning.inference import Inference
 
+pername_extractor_deeplearning = Inference()
 extractor = Extractor(load_dict=False) # True: Load dictionary path
 
 # text = '9h 20 phút '
@@ -11,3 +13,6 @@ extractor = Extractor(load_dict=False) # True: Load dictionary path
 
 text = 'tôi muốn đặt lịch với bs nguyễn nhật lệ lúc 8h sáng ngày 20/7'
 print(extractor.extract_ner(text, "book_appt"))
+
+x = pername_extractor_deeplearning.inference("đặt lịch với bác sĩ nguyễn huy tưởng lúc 9 giờ sáng mai")
+print(x)
