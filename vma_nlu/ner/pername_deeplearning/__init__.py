1b73b606994ae8d6c20cca2a8d580f6fd286ff91
Pham Nam <namph@ftech.ai>
2021-07-07 13:26:27 +0700

    update model_dir

--- vma_nlu/ner/pername_deeplearning/__init__.py
+++ vma_nlu/ner/pername_deeplearning/__init__.py
@@ -1,7 +1,7 @@
 
 from pathlib import Path
 
-MODEL_DIR = str(Path.home()) + "/.biaffine"
+MODEL_DIR = str(Path.home()) + "/.biaffine/"
 BASE_URL = "https://github.com/demdecuong/vma_nlu/releases/download/v1.3.2-a/"
 CHECKPOINT = "checkpoint.pth"
 LABEL_SET = "label_set.txt"
