42af0769bb192e9b8abf3d953cfcd5f2d907dd61
Pham Nam <namph@ftech.ai>
2021-07-30 09:06:02 +0700

    chore: wrap up project

--- setup.py
+++ setup.py
@@ -1,45 +1,17 @@
 import os
 from setuptools import setup, find_packages
-from os import path
 
-try:
-    # pip >=20
-    from pip._internal.network.session import PipSession
-    from pip._internal.req import parse_requirements
-except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip <= 9.0.3
-        from pip.download import PipSession
-        from pip.req import parse_requirements
-
-long_description = ''
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session=False)
-
-# read the contents of your README file
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-try:
-    reqs = [str(ir.req) for ir in install_reqs]
-except:
-    reqs = [str(ir.requirement) for ir in install_reqs]
-
+with open("requirements.txt") as f:
+    reqs = f.read().splitlines()
+    
+print(reqs)
 VERSION = os.getenv('PACKAGE_VERSION', 'v0.0.1')[1:]
 
 setup(
     name='vi_nlp_core',
     version='0.3.3',
     description='vi-core-nlp is a library that supports Vietnamese NER by pattern matching .',
-    long_description=long_description,
+    long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
     include_package_data=True,
     url='https://github.com/demdecuong/vi-core-nlp/tree/main',
@@ -51,7 +23,7 @@ setup(
     ],
     install_requires=reqs,
     keywords='vi_nlp_core',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     py_modules=['vi_nlp_core'],
 
 )
